29e2bfb26b8ac7dccc5c5c57264e9dd0f5a77650
Tyler Boston <tboston6789@gmail.com>
2021-04-26 15:12:12 -0500

    Version 1.3.1? - Added NewVCfromDesktop and added func to print inside

---
+++ GitHubXCodeTest1/NewVCfromDesktop.swift
@@ -0,0 +1,18 @@
+//
+//  NewVCfromDesktop.swift
+//  GitHubXCodeTest1
+//
+//  Created by Tyler Boston on 4/26/21.
+//
+
+import UIKit
+
+class NewVCFromDesktop: ViewController {
+    
+    func pocket() {
+        print("Coming from 1.3 pull from main into DesktopF1.3")
+        print("Would this be considered 1.3.1? Also, after merging branches to complete a version number we should prob create a new feature branch to indicate that everyonee should be caught up locally with 1.3")
+        
+    }
+}
+
