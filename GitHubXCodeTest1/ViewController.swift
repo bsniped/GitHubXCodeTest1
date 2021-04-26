b75947a2f11b501cd5fa88b05e1f206b2593ed34
Tyler Boston <tboston6789@gmail.com>
2021-04-26 14:55:57 -0500

    Version 1.3 - Merged both branches into main

--- GitHubXCodeTest1/ViewController.swift
+++ GitHubXCodeTest1/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("Hello World, This is version 1.2.6 committed to main repository")
+        print("Hello World, This is version 1.3 committed to main repository")
        print("In this version, we're adding a 'print' statement")
         
         
